e9b7c6183a0a57a43a5e5299c3a70f5798ac1f3e
AnaC <AnaC@192.168.1.206>
2018-04-19 10:11:00 +0100

    sin comentarios

--- src/modelo/Pila.java
+++ src/modelo/Pila.java
@@ -3,10 +3,8 @@ package modelo;
 import java.util.Stack;
 import modelo.Colores;
 
-
 public class Pila {
 
-	// Primero tenemos que instanciar la pila y decir de que va a ir rellena
 	Stack<Colores> pila = new Stack<Colores>();
 
 	// push
