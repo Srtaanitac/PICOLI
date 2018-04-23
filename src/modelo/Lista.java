f4fde946d0c14b7ef01e1ba4607ef675654b2d95
AnaC <AnaC@192.168.1.147>
2018-04-23 09:38:59 +0100

    comentario

--- src/modelo/Lista.java
+++ src/modelo/Lista.java
@@ -9,7 +9,7 @@ public class Lista {
 	public void alistar(Colores color) {
 		lista.add(color);
 	}
-
+//jsem emiliuo
 	public void desalistar() {
 		// eliminar el elemento en la posicion que le digamos 
 		lista.remove(this.lista.size());
